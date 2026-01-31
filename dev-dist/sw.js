d242d0e8624b6453ad6f467106af790d2d114d97
shrujal-srinath <shrujalsrinath@gmail.com>
2026-01-31 10:28:35 +0530

    shrujal3

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.e9o0946qke8"
+    "revision": "0.a8904e3akmg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
