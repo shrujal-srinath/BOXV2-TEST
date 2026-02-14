6eaaa1dca097b1fe4fc0b3140427b88abba50649
shrujal-srinath <shrujalsrinath@gmail.com>
2026-02-14 13:13:45 +0530

    esp error

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.6nsj1s091c"
+    "revision": "0.tr9gebjp1os"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
