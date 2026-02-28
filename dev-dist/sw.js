f04f53726b58bb85bc86bf781dc6a9f21926c5ec
shrujal-srinath <shrujalsrinath@gmail.com>
2026-02-28 12:35:05 +0530

    need to check the codes for esp linking and also for casting

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.i2ae0hs6tto"
+    "revision": "0.e6v7m1tabv"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
